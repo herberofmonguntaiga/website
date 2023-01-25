879b983751357b80a3e069b72e23237d9938309c
herberofmonguntaiga <123525429+herberofmonguntaiga@users.noreply.github.com>
2023-01-24 22:00:39 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+window.setInterval(() => {
+    const list = ["https://cdn.discordapp.com/attachments/787125869142605825/1067637249946169375/2Jelly_BW_-_Copy.png"]
+    document.getElementById("imagetwo").src = list[Math.floor(Math.random() * list.length)]
+}, 2000)
+
+document.getElementsByTagName("body")[0].addEventListener("click", function() {
+    document.getElementById("audio").play();
+})
